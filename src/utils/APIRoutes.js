bf57d4b6b164dee5b7c768b2a48424ad8ee9300e
Ritika Nagar <ritika20112@iiitd.ac.in>
2023-01-15 18:47:31 +0530

    last changes done

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://chatapp-api-ckqx.onrender.com";
+export const host = "https://apnichatapi.onrender.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
